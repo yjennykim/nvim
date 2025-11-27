2761f968b869ab0661e2ee6b1ad0d955cd31a8ec
jenny <jennykimcode@gmail.com>
2025-11-26 20:33:56 -0500

    modularize

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.opt.termguicolors = true 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -21,50 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim',
-     branch = 'master',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-  }
-}
 local opts = {}
 
-require("lazy").setup(plugins,opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', function()
-  require('telescope.builtin').find_files({
-    previewer = true,       -- enable preview window
-    hidden = true,          -- show hidden files (optional)
-  })
-end)
-
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- live grep
-vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
-
--- theme
-require("catppuccin").setup({
-  flavour = "mocha",
-})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-vim.cmd.colorscheme "catppuccin"
-
-
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = 'master',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<C-p>', function()
+      require('telescope.builtin').find_files({
+        previewer = true,       -- enable preview window
+        hidden = true,          -- show hidden files (optional)
+      })
+    end)
+
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- live grep
+    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
+  end
+}
